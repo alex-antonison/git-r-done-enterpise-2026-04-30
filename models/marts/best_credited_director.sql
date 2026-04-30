ff2e3fd7145bd06a5301bc518a77fcd436435d5b
TIL-harrietowen <harriet.owen@theinformationlab.com>
2026-04-30 17:24:18 +0100

    ordered directors descending by average imbd rating

--- models/marts/best_credited_director.sql
+++ models/marts/best_credited_director.sql
@@ -1,5 +1,6 @@
     SELECT
         director,
-        avg(imdb_rating)
+        avg(imdb_rating) as average_imdb_rating
     FROM {{ ref('stg_movies') }}
     group by director
+    order by average_imdb_rating
